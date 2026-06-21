768cdf6bfab74a38c7077c89271dcaacdacd1008
Deyjuwon <temitayoadejuwon@gmail.com>
2026-06-21 14:44:10 +0100

    Complete milestone 1 - products API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: {
+      error: "Product not found"
+    }, status: :not_found
+  end
 end
