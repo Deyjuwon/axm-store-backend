05af66004aeb41c7ef5567f13a1b2f4d607bde39
Deyjuwon <temitayoadejuwon@gmail.com>
2026-06-25 00:32:49 +0100

    fix: replace hardcoded product error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound do
     render json: {
-      error: "Product not found"
+      error: "Record not found"
     }, status: :not_found
   end
 end
