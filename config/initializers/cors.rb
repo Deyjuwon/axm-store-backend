88e1fc3d05222f727b57d79a83d5d37e3b4012f6
Deyjuwon <temitayoadejuwon@gmail.com>
2026-06-27 22:26:44 +0100

    refactor(cors): make allowed origin configurable

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
-    resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+    origins ENV.fetch("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+    resource "*", headers: :any, methods: [ :get, :options, :head ]
   end
 end
